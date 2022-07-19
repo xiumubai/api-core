af5f6caab0e0ce5467fb8f19af39440a982bd521
xiumubai <1547702880@qq.com>
2022-07-19 17:44:16 +0800

    feat: 克隆函数终结版本

---
+++ src/code/copy.js
@@ -0,0 +1,230 @@
+/** 可继续遍历的数据类型 */
+const mapTag = '[object Map]';
+const setTag = '[object Set]';
+const arrayTag = '[object Array]';
+const objectTag = '[object Object]';
+const argsTag = '[object Arguments]';
+/** 不可继续遍历的数据类型 */
+const boolTag = '[object Boolean]';
+const dateTag = '[object Date]';
+const errorTag = '[object Error]';
+const numberTag = '[object Number]';
+const regexpTag = '[object RegExp]';
+const stringTag = '[object String]';
+const symbolTag = '[object Symbol]';
+const funcTag = '[object Function]';
+
+const deepTag = [mapTag, setTag, arrayTag, objectTag, argsTag];
+
+/**
+ * @description: 使用while模拟forEach函数
+ * @param {*} array
+ * @param {*} iteratee
+ * @return {*}
+ */
+function forEach(array, iteratee) {
+  let index = -1;
+  const length = array.length;
+  while (++index < length) {
+    iteratee(array[index], index);
+  }
+  return array;
+}
+
+/**
+ * @description: 判断是否为引用类型
+ * @param {*} target
+ * @return {*}
+ */
+function isObject(target) {
+  const type = typeof target;
+  return target !== null && (type === 'object' || type === 'function');
+}
+
+/**
+ * @description: 获取数据类型
+ * @param {*} target
+ * @return {*}
+ */
+function getType(target) {
+  return Object.prototype.toString.call(target);
+}
+
+/**
+ * @description: 初始化被克隆的对象，保证原型被复制
+ * @param {*} target
+ * @return {*}
+ */
+function getInit(target) {
+  const Ctor = target.constructor;
+  return new Ctor();
+}
+
+/**
+ * @description: 克隆Symbol类型
+ * @param {*} target
+ * @return {*}
+ */
+function cloneSymbol(target) {
+  return Object(Symbol.prototype.valueOf.call(target));
+}
+
+/**
+ * @description: 克隆正则
+ * @param {*} target
+ * @return {*}
+ */
+function cloneReg(target) {
+  const reFlags = /\w*$/;
+  const result = new target.constructor(target.source, reFlags.exec(target));
+  result.lastIndex = target.lastIndex;
+  return result;
+}
+
+/**
+ * @description: 克隆函数部分功能拆分，用于区分普通函数和箭头函数
+ * @param {*} func
+ * @return {*}
+ */
+function cloneFunction(func) {
+  const bodyReg = /(?<={)(.|\n)+(?=})/m;
+  const paramReg = /(?<=\().+(?=\)\s+{)/;
+  const funcString = func.toString();
+  if (func.prototype) {
+    const param = paramReg.exec(funcString);
+    const body = bodyReg.exec(funcString);
+    if (body) {
+      // console.log('匹配到函数体：', body[0]);
+      if (param) {
+        const paramArr = param[0].split(',');
+        // console.log('匹配到参数：', paramArr);
+        return new Function(...paramArr, body[0]);
+      } else {
+        return new Function(body[0]);
+      }
+    } else {
+      return null;
+    }
+  } else {
+    return eval(funcString);
+  }
+}
+
+/**
+ * @description: 克隆不可遍历的数据类型
+ * @param {*} target
+ * @param {*} type
+ * @return {*}
+ */
+function cloneOtherType(target, type) {
+  const Ctor = target.constructor;
+  switch (type) {
+    case boolTag:
+    case numberTag:
+    case stringTag:
+    case errorTag:
+    case dateTag:
+      return new Ctor(target);
+    case regexpTag:
+      return cloneReg(target);
+    case symbolTag:
+      return cloneSymbol(target);
+    case funcTag:
+      return cloneFunction(target);
+    default:
+      return null;
+  }
+}
+
+/**
+ * @description: 成型的克隆函数
+ * @param {*} target
+ * @param {*} map
+ * @return {*}
+ */
+function clone(target, map = new WeakMap()) {
+  // 克隆原始类型
+  if (!isObject(target)) {
+    return target;
+  }
+
+  // 根据不同的数据类型进行克隆操作
+  const type = getType(target);
+  let cloneTarget;
+  if (deepTag.includes(type)) {
+    cloneTarget = getInit(target, type);
+  } else {
+    return cloneOtherType(target, type);
+  }
+
+  // 防止循环引用
+  if (map.get(target)) {
+    return map.get(target);
+  }
+  map.set(target, cloneTarget);
+
+  // 克隆set
+  if (type === setTag) {
+    target.forEach((value) => {
+      cloneTarget.add(clone(value, map));
+    });
+    return cloneTarget;
+  }
+
+  // 克隆map
+  if (type === mapTag) {
+    target.forEach((value, key) => {
+      cloneTarget.set(key, clone(value, map));
+    });
+    return cloneTarget;
+  }
+
+  // 克隆对象和数组
+  const keys = type === arrayTag ? undefined : Object.keys(target);
+  forEach(keys || target, (value, key) => {
+    if (keys) {
+      key = value;
+    }
+    cloneTarget[key] = clone(target[key], map);
+  });
+
+  return cloneTarget;
+}
+
+// 测试用例
+
+const map = new Map();
+map.set('key', 'value');
+map.set('ConardLi', 'code秘密花园');
+
+const set = new Set();
+set.add('ConardLi');
+set.add('code秘密花园');
+
+const target = {
+  field1: 1,
+  field2: undefined,
+  field3: {
+    child: 'child',
+  },
+  field4: [2, 4, 8],
+  empty: null,
+  map,
+  set,
+  bool: new Boolean(true),
+  num: new Number(2),
+  str: new String(2),
+  symbol: Object(Symbol(1)),
+  date: new Date(),
+  reg: /\d+/,
+  error: new Error(),
+  func1: () => {
+    console.log('code秘密花园');
+  },
+  func2: function (a, b) {
+    return a + b;
+  },
+};
+
+const result = clone(target);
+console.log(result);
--- src/js基础/cloneDeep.js
+++ src/js基础/cloneDeep.js
@@ -1,54 +1,65 @@
-/*
- * @Author: 朽木白
- * @Date: 2022-06-15 16:06:02
- * @LastEditors: 1547702880@qq.com
- * @LastEditTime: 2022-06-15 16:30:02
- * @Description:深拷贝的实现方式:
+/**
+ * 判断是否是基本数据类型
+ * @param value
  */
-
-// (1) JSON.parse(JSON.stringify())
-// 问题：不能处理函数和正则，基于JSON.stringify和JSON.parse处理后，得到的正则变为空对象，得到的函数变为null了。
-let arr5 = [
-  1,
-  3,
-  {
-    username: ' kobe',
-  },
-];
-let arr6 = JSON.parse(JSON.stringify(arr5));
-arr5[2].username = 'duncan';
-console.log(5, arr5);
-
-// (3) 函数库lodash的_.cloneDeep方法
-
-var _ = require('lodash');
-var obj1 = {
-  a: 1,
-  b: { f: { g: 1 } },
-  c: [1, 2, 3],
-};
-var obj2 = _.cloneDeep(obj1);
-console.log(obj1.b.f === obj2.b.f); // false
-
-// (2) 手写递归方法
-// 递归方法实现深度克隆原理：遍历对象、数组直到里边都是基本数据类型，然后再去复制，就是深度拷贝。
-// 对象存在循环引用的情况：
-// 额外开辟一个存储空间，储当前对象和拷贝对象的对应关系
-// 当需要拷贝当前对象时，先去存储空间中找，有没有拷贝过这个对象，
-// 如果有的话直接返回，如果没有的话继续拷贝，这样就巧妙化解的循环引用的问题。
-
-function cloneDeep(object) {
-  if (!object || typeof object !== 'object') return;
-
-  let newObject = Array.isArray(object) ? [] : {};
-
-  for (let key in object) {
-    if (object.hasOwnproperty(key)) {
-      newobject[key] =
-        typeof newObject[key] === 'object'
-          ? cloneDeep(object[key])
-          : object[key];
-    }
-  }
-  return newObject;
+function isPrimitive(value) {
+  return (
+    typeof value === 'string' ||
+    typeof value === 'number' ||
+    typeof value === 'symbol' ||
+    typeof value === 'boolean'
+  );
 }
+
+/**
+ * 判断是否是一个js对象
+ * @param value
+ */
+function isObject(value) {
+  return Object.prototype.toString.call(value) === '[object Object]';
+}
+
+/**
+ * 深拷贝一个值（包含对象的循环引用）
+ * @param value
+ */
+function cloneDeep(value) {
+  // 记录被拷贝的值，避免循环引用的出现
+  let memo = {};
+  function baseClone(value) {
+    let res;
+    // 如果是基本数据类型，则直接返回
+    if (isPrimitive(value)) {
+      return value;
+    } else if (Array.isArray(value)) {
+      // 如果是引用数据类型，我们浅拷贝一个新值来代替原来的值
+      res = [...value];
+    } else if (isObject(value)) {
+      res = { ...value };
+    }
+    // 检测我们浅拷贝的这个对象的属性值有没有是引用数据类型。如果是，则递归拷贝
+    Reflect.ownKeys(res).forEach((key) => {
+      if (typeof res[key] === 'object' && res[key] !== null) {
+        //此处我们用memo来记录已经被拷贝过的引用地址。以此来解决循环引用的问题
+        if (memo[res[key]]) {
+          res[key] = memo[res[key]];
+        } else {
+          memo[res[key]] = res[key];
+          res[key] = baseClone(res[key]);
+        }
+      }
+    });
+    return res;
+  }
+  return baseClone(value);
+}
+
+// 测试
+
+var obj = { a: 1, c: 2 };
+var b = { obj };
+
+obj.b = b;
+
+var copy = cloneDeep(obj);
+console.log(copy);
